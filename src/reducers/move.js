bd4596b0a64e27881e07cbcdeff2bbf9324ae918
Brent Guistwite <brentguistwite@gmail.com>
2018-02-13 09:37:59 -0500

    CHESS CONVERTER WORKING. Use function to transform FEN to algebraic

--- src/reducers/move.js
+++ src/reducers/move.js
@@ -2,13 +2,14 @@ import Chess from 'chess.js';
 import { MAKE_MOVE } from '../actions';
 
 const initialState = new Chess();
-console.log(initialState);
+console.log(initialState.board());
 
 const reducer = (state = initialState, action) => {
   if (action.type === MAKE_MOVE) {
     if (state.move(action.move)) {
-      console.log(state.ascii());
-      console.log(state.moves());
+      console.log(state.board());
+      const moveInfo = state.history({ verbose: true });
+      console.log(moveInfo);
     } else console.log('invalid move');
   }
 };
