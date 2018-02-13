805964b28ef10f37ff4fa4618ed0000166fd503e
Brent Guistwite <brentguistwite@gmail.com>
2018-02-13 10:11:19 -0500

    set up initial board in state

--- src/ChessGame.js
+++ src/ChessGame.js
@@ -11,6 +11,10 @@ export class ChessGame extends React.Component {
     this.chess = new Chess();
   }
 
+  onDragStart(piece) {
+    console.log(piece.notation);
+  }
+
   onMovePiece(piece, from, to) {
     console.log(this.props);
     const moveObj = { from, to };
@@ -18,11 +22,45 @@ export class ChessGame extends React.Component {
   }
 
   render() {
+    const pieces = ['R@a1',
+      'N@b1',
+      'B@c1',
+      'Q@d1',
+      'K@e1',
+      'B@f1',
+      'N@g1',
+      'R@h1',
+      'P@a2',
+      'P@b2',
+      'P@c2',
+      'P@d2',
+      'P@e2',
+      'P@f2',
+      'P@g2',
+      'P@h2',
+      'p@a7',
+      'p@b7',
+      'p@c7',
+      'p@d7',
+      'p@e7',
+      'p@f7',
+      'p@g7',
+      'p@h7',
+      'r@a8',
+      'n@b8',
+      'b@c8',
+      'q@d8',
+      'k@e8',
+      'b@f8',
+      'n@g8',
+      'r@h8'];
     return (
       <ChessBoard
         onMovePiece={(piece, fromSquare, toSquare) =>
           this.onMovePiece(piece, fromSquare, toSquare)}
-
+        pieces={pieces}
+        onDragStart={(piece, fromSquare) =>
+          this.onDragStart(piece, fromSquare)}
       />
     );
   }
